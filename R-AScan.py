4f44abd0373d273dd0e7479f8d6432f008b83030
afrzlfa <afrzlf4@gmail.com>
2025-06-13 14:39:11 +0700

    Update

--- R-AScan.py
+++ R-AScan.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     if not args.update:
-        print("[!] A target must be specified unless the --update option is used.\n")
+        print("[-] [A target must be specified unless the --update option is used]\n")
         parser.print_help()
         exit()
 
