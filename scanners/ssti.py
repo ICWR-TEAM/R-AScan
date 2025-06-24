e89e2cc14ceb2339fc99e5701fee6c17b06dba6f
Afrizal F.A <77115748+HarshXor@users.noreply.github.com>
2025-06-24 10:10:20 +0700

    Update ssti.py

--- scanners/ssti.py
+++ scanners/ssti.py
@@ -1,7 +1,9 @@
 import requests
+import os
 from urllib.parse import urljoin, urlencode
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from config import HTTP_HEADERS, DEFAULT_TIMEOUT
+from module.other import Other
 
 class SSTIScanner:
     SSTI_PAYLOADS = [
@@ -29,10 +31,13 @@ class SSTIScanner:
         self.thread = args.threads
         self.session = requests.Session()
         self.session.headers.update(HTTP_HEADERS)
+        self.module_name = os.path.splitext(os.path.basename(__file__))[0]
+        self.printer = Other()
 
     def scan(self):
         results = []
         tasks = []
+        colored_module = self.printer.color_text(self.module_name, "cyan")
 
         with ThreadPoolExecutor(max_workers=self.thread) as executor:
             for endpoint in self.COMMON_ENDPOINTS:
@@ -47,7 +52,12 @@ class SSTIScanner:
             for future in as_completed(tasks):
                 res = future.result()
                 if res:
+                    colored_endpoint = self.printer.color_text(res['endpoint'], "yellow")
+                    print(f"[!] [Module: {colored_module}] SSTI detected on {colored_endpoint} param={res['param']} payload={res['payload']}")
                     results.append(res)
+
+        if not results:
+            print(f"[*] [Module: {colored_module}] No SSTI vulnerabilities detected.")
 
         return {"target": self.target, "ssti_findings": results}
 
@@ -70,7 +80,7 @@ class SSTIScanner:
                     "match": self._match_output(payload, r.text)
                 }
 
-        except:
+        except Exception:
             return None
 
     def _is_ssti_success(self, payload, response_text):
