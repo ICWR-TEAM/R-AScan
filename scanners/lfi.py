270507684c01642857bf7690b0ead243bb77e461
Afrizal F.A <77115748+HarshXor@users.noreply.github.com>
2025-07-04 13:14:46 +0700

    Update lfi.py

--- scanners/lfi.py
+++ scanners/lfi.py
@@ -9,7 +9,7 @@ from module.other import Other
 
 class LFIScanner:
     def __init__(self, args):
-        self.target = args.target
+        self.target = f"{args.target}:{args.port}" if args.port else args.target
         self.threads = args.threads
         self.verbose = args.verbose
         self.module_name = os.path.splitext(os.path.basename(__file__))[0]
