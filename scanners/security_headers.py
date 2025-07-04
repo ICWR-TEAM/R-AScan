e26cad5b0c0bda4d38c92e18e7543ac3d9ffa5a8
Afrizal F.A <77115748+HarshXor@users.noreply.github.com>
2025-07-04 13:13:23 +0700

    Update security_headers.py

--- scanners/security_headers.py
+++ scanners/security_headers.py
@@ -4,7 +4,7 @@ from module.other import Other
 
 class SecurityHeaderScanner:
     def __init__(self, args):
-        self.target = args.target
+        self.target = f"{args.target}:{args.port}" if args.port else args.target
         self.verbose = args.verbose
         self.required_headers = [
             "Content-Security-Policy",
